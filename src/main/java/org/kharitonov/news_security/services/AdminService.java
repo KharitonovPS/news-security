1f7531be67cd388af26d05ad11c2b973bcc86c08
KharitonovPS <pasyaoo@gmail.com>
2024-02-04 22:54:58 +0300

    add adminService to change role of user

---
+++ src/main/java/org/kharitonov/news_security/services/AdminService.java
@@ -0,0 +1,41 @@
+package org.kharitonov.news_security.services;
+
+import org.kharitonov.news_security.models.ROLES;
+import org.kharitonov.news_security.models.User;
+import org.kharitonov.news_security.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @author Kharitonov Pavel on 04.02.2024.
+ */
+@Service
+public class AdminService {
+
+    private final UserRepository repository;
+
+    public AdminService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<HttpStatus> makeAdmin(String username) {
+        Optional<User> user = repository.findByUsername(username);
+        if (user.isPresent()) {
+            User updateUser = new User(user.get().getId(),
+                    username,
+                    user.get().getPassword(),
+                    ROLES.ROLE_ADMIN
+            );
+            repository.save(updateUser);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
